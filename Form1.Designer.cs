091751d69d950527741c6a5874deb2c4c8e4b328
Emanuel Nadir Hamui <hamuiemanuel@gmail.com>
2024-11-26 18:08:14 -0300

    pushhh

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            lblSistema = new Label();
+            btnAdminBanco = new Button();
+            btnGenerarExm = new Button();
+            btnRespuestas = new Button();
+            btnCorregir = new Button();
+            SuspendLayout();
+            // 
+            // lblSistema
+            // 
+            lblSistema.AutoSize = true;
+            lblSistema.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblSistema.Location = new Point(230, 72);
+            lblSistema.Name = "lblSistema";
+            lblSistema.Size = new Size(291, 25);
+            lblSistema.TabIndex = 0;
+            lblSistema.Text = "Sistema de Gestión de Exámenes:";
+            // 
+            // btnAdminBanco
+            // 
+            btnAdminBanco.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnAdminBanco.Location = new Point(183, 128);
+            btnAdminBanco.Name = "btnAdminBanco";
+            btnAdminBanco.Size = new Size(159, 57);
+            btnAdminBanco.TabIndex = 1;
+            btnAdminBanco.Text = "Administración del Banco de preguntas. ";
+            btnAdminBanco.UseVisualStyleBackColor = true;
+            btnAdminBanco.Click += btnAdminBanco_Click;
+            // 
+            // btnGenerarExm
+            // 
+            btnGenerarExm.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnGenerarExm.Location = new Point(183, 219);
+            btnGenerarExm.Name = "btnGenerarExm";
+            btnGenerarExm.Size = new Size(159, 57);
+            btnGenerarExm.TabIndex = 5;
+            btnGenerarExm.Text = "Generación de Exámenes.";
+            btnGenerarExm.UseVisualStyleBackColor = true;
+            btnGenerarExm.Click += btnGenerarExm_Click;
+            // 
+            // btnRespuestas
+            // 
+            btnRespuestas.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnRespuestas.Location = new Point(395, 128);
+            btnRespuestas.Name = "btnRespuestas";
+            btnRespuestas.Size = new Size(159, 57);
+            btnRespuestas.TabIndex = 6;
+            btnRespuestas.Text = "Respuestas Exámen.";
+            btnRespuestas.UseVisualStyleBackColor = true;
+            btnRespuestas.Click += btnRespuestas_Click;
+            // 
+            // btnCorregir
+            // 
+            btnCorregir.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnCorregir.Location = new Point(395, 219);
+            btnCorregir.Name = "btnCorregir";
+            btnCorregir.Size = new Size(159, 57);
+            btnCorregir.TabIndex = 7;
+            btnCorregir.Text = "Correción Exámenes.";
+            btnCorregir.UseVisualStyleBackColor = true;
+            btnCorregir.Click += btnCorregir_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(739, 411);
+            Controls.Add(btnCorregir);
+            Controls.Add(btnRespuestas);
+            Controls.Add(btnGenerarExm);
+            Controls.Add(btnAdminBanco);
+            Controls.Add(lblSistema);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblSistema;
+        private Button btnAdminBanco;
+        private Button button4;
+        private Button btnGenerarExm;
+        private Button btnRespuestas;
+        private Button btnCorregir;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,29 @@ namespace TrabajoFinal_
         {
             InitializeComponent();
         }
+
+        private void btnAdminBanco_Click(object sender, EventArgs e)
+        {
+            Form2 ventana = new Form2();
+            ventana.Visible = true;
+        }
+
+        private void btnRespuestas_Click(object sender, EventArgs e)
+        {
+            Form3 ventana = new Form3();
+            ventana.Visible = true;
+        }
+
+        private void btnGenerarExm_Click(object sender, EventArgs e)
+        {
+            Form4 ventana = new Form4();
+            ventana.Visible = true;
+        }
+
+        private void btnCorregir_Click(object sender, EventArgs e)
+        {
+            Form5 ventana = new Form5();
+            ventana.Visible = true;
+        }
     }
 }
