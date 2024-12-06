5570df31674d46b9b9a9da97910c7d0b36526f99
Emanuel Nadir Hamui <hamuiemanuel@gmail.com>
2024-12-06 20:23:46 -0300

    Boton Modificar andando.

--- FrmModificarPregunta.Designer.cs
+++ FrmModificarPregunta.Designer.cs
@@ -31,7 +31,7 @@
             btnAceptar = new Button();
             btnVolver = new Button();
             lblPreguntaID = new Label();
-            label2 = new Label();
+            cmbNumeroRespuesta = new ComboBox();
             label3 = new Label();
             label4 = new Label();
             label5 = new Label();
@@ -46,21 +46,23 @@
             txtAsignatura = new TextBox();
             txtRespuestaCorrecta = new TextBox();
             txtRespuesta = new TextBox();
-            cmbNumeroRespuesta = new ComboBox();
+            label2 = new Label();
+            btnBuscarIDpregunta = new Button();
             SuspendLayout();
             // 
             // btnAceptar
             // 
-            btnAceptar.Location = new Point(280, 397);
+            btnAceptar.Location = new Point(195, 397);
             btnAceptar.Name = "btnAceptar";
             btnAceptar.Size = new Size(75, 23);
             btnAceptar.TabIndex = 0;
             btnAceptar.Text = "Aceptar";
             btnAceptar.UseVisualStyleBackColor = true;
+            btnAceptar.Click += btnAceptar_Click;
             // 
             // btnVolver
             // 
-            btnVolver.Location = new Point(410, 397);
+            btnVolver.Location = new Point(347, 397);
             btnVolver.Name = "btnVolver";
             btnVolver.Size = new Size(75, 23);
             btnVolver.TabIndex = 1;
@@ -71,83 +73,91 @@
             // lblPreguntaID
             // 
             lblPreguntaID.AutoSize = true;
+            lblPreguntaID.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblPreguntaID.ForeColor = SystemColors.ControlText;
             lblPreguntaID.Location = new Point(152, 59);
             lblPreguntaID.Name = "lblPreguntaID";
-            lblPreguntaID.Size = new Size(69, 15);
+            lblPreguntaID.Size = new Size(93, 20);
             lblPreguntaID.TabIndex = 2;
             lblPreguntaID.Text = "ID Pregunta";
             // 
-            // label2
+            // cmbNumeroRespuesta
             // 
-            label2.AutoSize = true;
-            label2.Location = new Point(152, 97);
-            label2.Name = "label2";
-            label2.Size = new Size(55, 15);
-            label2.TabIndex = 3;
-            label2.Text = "Pregunta";
+            cmbNumeroRespuesta.FormattingEnabled = true;
+            cmbNumeroRespuesta.Location = new Point(234, 176);
+            cmbNumeroRespuesta.Name = "cmbNumeroRespuesta";
+            cmbNumeroRespuesta.Size = new Size(55, 23);
+            cmbNumeroRespuesta.TabIndex = 24;
             // 
             // label3
             // 
             label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label3.Location = new Point(152, 138);
             label3.Name = "label3";
-            label3.Size = new Size(108, 15);
+            label3.Size = new Size(146, 20);
             label3.TabIndex = 4;
             label3.Text = "Lista de Respuestas";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(152, 179);
+            label4.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(143, 175);
             label4.Name = "label4";
-            label4.Size = new Size(77, 15);
+            label4.Size = new Size(85, 20);
             label4.TabIndex = 5;
-            label4.Text = "Respuesta N°";
+            label4.Text = "Respuesta ";
             // 
             // label5
             // 
             label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label5.Location = new Point(152, 221);
             label5.Name = "label5";
-            label5.Size = new Size(106, 15);
+            label5.Size = new Size(142, 20);
             label5.TabIndex = 6;
             label5.Text = "Respuesta correcta";
             // 
             // label6
             // 
             label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label6.Location = new Point(152, 263);
             label6.Name = "label6";
-            label6.Size = new Size(64, 15);
+            label6.Size = new Size(86, 20);
             label6.TabIndex = 7;
             label6.Text = "Asignatura";
             // 
             // label1
             // 
             label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label1.Location = new Point(152, 304);
             label1.Name = "label1";
-            label1.Size = new Size(45, 15);
+            label1.Size = new Size(59, 20);
             label1.TabIndex = 8;
             label1.Text = "Unidad";
             // 
             // lblSubUnidadModificar
             // 
             lblSubUnidadModificar.AutoSize = true;
+            lblSubUnidadModificar.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblSubUnidadModificar.Location = new Point(152, 344);
             lblSubUnidadModificar.Name = "lblSubUnidadModificar";
-            lblSubUnidadModificar.Size = new Size(70, 15);
+            lblSubUnidadModificar.Size = new Size(91, 20);
             lblSubUnidadModificar.TabIndex = 9;
             lblSubUnidadModificar.Text = "Sub-Unidad";
             // 
             // btnModificarRespuesta
             // 
-            btnModificarRespuesta.Location = new Point(465, 175);
+            btnModificarRespuesta.Location = new Point(451, 176);
             btnModificarRespuesta.Name = "btnModificarRespuesta";
             btnModificarRespuesta.Size = new Size(75, 23);
             btnModificarRespuesta.TabIndex = 10;
             btnModificarRespuesta.Text = "Modificar";
             btnModificarRespuesta.UseVisualStyleBackColor = true;
+            btnModificarRespuesta.Click += btnModificarRespuesta_Click;
             // 
             // txtIDPregunta
             // 
@@ -198,19 +208,32 @@
             txtRespuesta.Size = new Size(100, 23);
             txtRespuesta.TabIndex = 23;
             // 
-            // cmbNumeroRespuesta
+            // label2
             // 
-            cmbNumeroRespuesta.FormattingEnabled = true;
-            cmbNumeroRespuesta.Location = new Point(234, 176);
-            cmbNumeroRespuesta.Name = "cmbNumeroRespuesta";
-            cmbNumeroRespuesta.Size = new Size(55, 23);
-            cmbNumeroRespuesta.TabIndex = 24;
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(152, 97);
+            label2.Name = "label2";
+            label2.Size = new Size(73, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Pregunta";
+            // 
+            // btnBuscarIDpregunta
+            // 
+            btnBuscarIDpregunta.Location = new Point(429, 61);
+            btnBuscarIDpregunta.Name = "btnBuscarIDpregunta";
+            btnBuscarIDpregunta.Size = new Size(75, 23);
+            btnBuscarIDpregunta.TabIndex = 25;
+            btnBuscarIDpregunta.Text = "Buscar ID";
+            btnBuscarIDpregunta.UseVisualStyleBackColor = true;
+            btnBuscarIDpregunta.Click += btnBuscarIDpregunta_Click;
             // 
             // FrmModificarPregunta
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(667, 441);
+            Controls.Add(btnBuscarIDpregunta);
             Controls.Add(cmbNumeroRespuesta);
             Controls.Add(txtRespuesta);
             Controls.Add(txtRespuestaCorrecta);
@@ -239,11 +262,14 @@
         }
 
         #endregion
-
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
         private Button btnAceptar;
         private Button btnVolver;
         private Label lblPreguntaID;
-        private Label label2;
+        private ComboBox cmbNumeroRespuesta;
         private Label label3;
         private Label label4;
         private Label label5;
@@ -253,15 +279,12 @@
         private Button btnModificarRespuesta;
         private TextBox txtIDPregunta;
         private TextBox txtPregunta;
-        private TextBox txtRespuesta;
-        private TextBox textBox4;
-        private TextBox textBox5;
-        private TextBox textBox6;
-        private TextBox textBox7;
         private TextBox txtSubUnidad;
         private TextBox txtUnidad;
         private TextBox txtAsignatura;
         private TextBox txtRespuestaCorrecta;
-        private ComboBox cmbNumeroRespuesta;
+        private TextBox txtRespuesta;
+        private Label label2;
+        private Button btnBuscarIDpregunta;
     }
 }
--- FrmModificarPregunta.cs
+++ FrmModificarPregunta.cs
@@ -1,7 +1,14 @@
-﻿namespace TrabajoFinal_
+﻿using System.Text.Json;
+
+namespace TrabajoFinal_
 {
     public partial class FrmModificarPregunta : Form
     {
+        const string CARPETA = "files";
+        string rutaArchivo = Path.Combine(CARPETA, "Preguntas.json");
+
+        private List<Pregunta> preguntas = new List<Pregunta>();
+        private List<string> Respuestas = new List<string>();
 
         public FrmModificarPregunta()
         {
@@ -12,6 +19,93 @@
         private void btnVolver_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+        private void CargarBancoPreguntas()
+        {
+            try
+            {
+
+                string Json = File.ReadAllText(rutaArchivo);
+
+                preguntas = JsonSerializer.Deserialize<List<Pregunta>>(Json) ?? new List<Pregunta>();
+
+                foreach (var pregunta in preguntas)
+                {
+                    if (pregunta.PreguntaId.ToString() == txtIDPregunta.Text)
+                    {
+                        txtPregunta.Text = pregunta.TxtPregunta;
+                        txtRespuestaCorrecta.Text = pregunta.RespuestaCorrecta.ToString();
+                        txtAsignatura.Text = pregunta.Asignatura;
+                        txtUnidad.Text = pregunta.Unidad;
+                        txtSubUnidad.Text = pregunta.SubUnidad;
+                        foreach (var Respuesta in pregunta.ListaDeRespuestas)
+                        {
+                            cmbNumeroRespuesta.Items.Add(Respuesta);
+                        }
+
+                    }
+                }
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar Json", ex.Message);
+            }
+        }
+
+        private void btnBuscarIDpregunta_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                CargarBancoPreguntas();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al busca Id!", ex.Message);
+            }
+        }
+
+        private void btnModificarRespuesta_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int IndicieElegido = cmbNumeroRespuesta.SelectedIndex;
+
+                string NuevaRespuesta = txtRespuesta.Text;
+
+                string Json = File.ReadAllText(rutaArchivo);
+                preguntas = JsonSerializer.Deserialize<List<Pregunta>>(Json) ?? new List<Pregunta>();
+
+                cmbNumeroRespuesta.Items[IndicieElegido] = NuevaRespuesta;
+                MessageBox.Show("La respuesta se ha modificado Correctamente");
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al Modificar!", ex.Message);
+            }
+
+
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+
+                string Nuevojson = JsonSerializer.Serialize(preguntas, new JsonSerializerOptions { WriteIndented = true });
+                File.WriteAllText(rutaArchivo, Nuevojson);
+                MessageBox.Show("Datos guardados correctamente en el archivo JSON.");
+
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al Modificar!", ex.Message);
+            }
         }
     }
 }
